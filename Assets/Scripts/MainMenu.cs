256247ea5271d3af17e1255a491bc14b92e88fdd
WesleyWi <81112138+WesleyWi@users.noreply.github.com>
2023-09-17 18:29:12 -0500

    WorkOnMainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
