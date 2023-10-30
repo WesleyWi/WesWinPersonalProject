3f50b223d1ac05c1be11197a1042eb06c7f531c9
WesleyWi <81112138+WesleyWi@users.noreply.github.com>
2023-10-30 08:40:41 -0500

    Fixes

--- Assets/Scripts/Input/ShootingScript.cs
+++ Assets/Scripts/Input/ShootingScript.cs
@@ -3,143 +3,85 @@ using UnityEngine.InputSystem;
 
 public class ShootingScript : MonoBehaviour
 {
-    [Header("Shooting Settings")]
-    public Transform gunTransform; // Assign your gun's transform in the Inspector.
-    public GameObject projectilePrefab; // Assign your projectile prefab in the Inspector.
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public Camera playerCamera; // Reference to the player's camera
     public float shootForce = 10f;
-    public float maxShootDistance = 5f; // Maximum shooting distance in feet.
 
-    [Header("Pickup Settings")]
-    public float pickupDistance = 2f; // Maximum pickup distance in feet.
-    private GameObject heldObject; // The object currently held by the player.
-
-    private MyInputAction inputActions;
-
-    private void Awake()
-    {
-        inputActions = new MyInputAction();
-        inputActions.Player.Shoot.performed += _ => TryShoot();
-        inputActions.Player.Pickup.performed += _ => TogglePickupDrop();
-    }
-
-    private void OnEnable()
-    {
-        inputActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        inputActions.Disable();
-    }
+    private GameObject heldObject = null;
+    private bool isHoldingObject = false;
 
     private void Update()
     {
-        // Handle held object's positioning and following the camera.
-        if (heldObject != null)
-        {
-            PositionHeldObject();
-            FollowCamera();
-        }
-    }
-    private void TogglePickupDrop()
-    {
-        if (heldObject == null)
-        {
-            TryPickup();
-        }
-        else
-        {
-            DropHeldObject();
-        }
-    }
-
-    private void TryPickup()
-    {
-        // Check if a "box" is in front of the player and within pickupDistance.
-        Ray ray = new Ray(gunTransform.position, gunTransform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance))
-        {
-            if (hit.collider.CompareTag("box"))
-            {
-                // Set the heldObject to the picked-up box.
-                heldObject = hit.collider.gameObject;
-                // Optionally, you can disable the box's physics or renderers to make it look like it's picked up.
-                heldObject.GetComponent<Rigidbody>().isKinematic = true;
-                heldObject.GetComponent<Collider>().enabled = false;
-            }
-        }
-    }
-
-    private void DropHeldObject()
-    {
-        if (heldObject != null)
-        {
-            // Release the held object. You can re-enable its physics and colliders here.
-            heldObject.GetComponent<Rigidbody>().isKinematic = false;
-            heldObject.GetComponent<Collider>().enabled = true;
-            heldObject = null;
-        }
-    }
-
-    private void TryShoot()
-    {
-        // Check if there's an obstacle within maxShootDistance
-        if (CanShoot())
+        if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Shoot();
         }
-        else
+
+        if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            // You can provide feedback to the player here, like a message or sound indicating that shooting is blocked.
-            Debug.Log("Obstacle in the way or target is too close to shoot.");
+            if (!isHoldingObject)
+            {
+                TryPickUp();
+            }
+            else
+            {
+                DropHeldObject();
+            }
         }
     }
 
     private void Shoot()
     {
-        if (gunTransform != null && projectilePrefab != null)
+        GameObject newProjectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
+        if (rb != null)
         {
-            GameObject newProjectile = Instantiate(projectilePrefab, gunTransform.position, gunTransform.rotation);
-            Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
-            rb.AddForce(gunTransform.forward * shootForce, ForceMode.VelocityChange);
-
-            // Display a debug message when you shoot
-            Debug.Log("Shot fired!");
+            rb.AddForce(firePoint.forward * shootForce, ForceMode.VelocityChange);
         }
     }
 
-    private bool CanShoot()
+    private void TryPickUp()
     {
-        if (gunTransform != null && projectilePrefab != null)
+        RaycastHit hit;
+        Ray ray = playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out hit, 10f))
         {
-            // Create a ray from the gun position in the direction the gun is pointing
-            Ray ray = new Ray(gunTransform.position, gunTransform.forward);
-
-            if (Physics.Raycast(ray, out RaycastHit hit, maxShootDistance))
+            if (hit.collider.CompareTag("box"))
             {
-
+                Debug.Log("Picked up " + hit.collider.name);
+                PickUpObject(hit.collider.gameObject);
             }
         }
-
-        return true; // Shooting is allowed if no obstacles are blocking the line of sight.
     }
 
-    private void PositionHeldObject()
+    private void PickUpObject(GameObject objToPickUp)
     {
-        if (gunTransform != null && heldObject != null)
-        {
-            // Offset the held object slightly in front of the gun.
-            Vector3 holdPosition = gunTransform.position + gunTransform.forward * 1.5f; // Adjust the distance as needed.
-            heldObject.transform.position = holdPosition;
-        }
+        isHoldingObject = true;
+        heldObject = objToPickUp;
+
+        Debug.Log("Holding " + heldObject.name);
+
+        // You can customize the behavior here, like attaching the object to the camera's forward direction.
+        heldObject.transform.parent = playerCamera.transform;
+        heldObject.GetComponent<Rigidbody>().isKinematic = true;
     }
 
-    private void FollowCamera()
+    private void DropHeldObject()
     {
-        if (gunTransform != null && heldObject != null)
+        Debug.Log("Dropped " + heldObject.name);
+
+        // You can customize the behavior here, like releasing the object.
+        heldObject.transform.parent = null;
+        Rigidbody rb = heldObject.GetComponent<Rigidbody>();
+        if (rb != null)
         {
-            // Make the held object always face the gun.
-            heldObject.transform.LookAt(gunTransform);
+            rb.isKinematic = false;
+            rb.velocity = Vector3.zero; // Set velocity to zero so it doesn't launch
         }
+
+        heldObject = null;
+        isHoldingObject = false;
     }
 }
